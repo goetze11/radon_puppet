78200844aea0c0b676889c26895abf031078189f
goetze11 <goetzec@seattleu.edu>
2017-07-25 14:45:18 -0700

    Configured e5

--- copyPuppetFiles.sh
+++ copyPuppetFiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-folderNum=4
+folderNum=5
 mkdir ~/radon_puppet/etc_$folderNum/sysconfig
 mkdir ~/radon_puppet/etc_$folderNum/sysconfig/network-scripts
 cp /etc/fstab ~/radon_puppet/etc_$folderNum
