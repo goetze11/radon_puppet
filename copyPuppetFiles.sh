772f106c63dd8932b686bfbc589369151ad18c29
goetze11 <goetzec@seattleu.edu>
2017-07-25 15:11:07 -0700

    Configured e7

--- copyPuppetFiles.sh
+++ copyPuppetFiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-folderNum=6
+folderNum=7
 mkdir ~/radon_puppet/etc_$folderNum/sysconfig
 mkdir ~/radon_puppet/etc_$folderNum/sysconfig/network-scripts
 cp /etc/fstab ~/radon_puppet/etc_$folderNum
---
+++ donnyFiles/donny_7.pp
@@ -0,0 +1,92 @@
+node "radoni.llnl.gov" {
+
+file { '/etc/hosts':
+    ensure => "file",
+    owner  => "root",
+    group  => "root",
+    mode   => "644",
+    source => "/root/radon_puppet/etc_mgmt/hosts",}  
+
+file { '/etc/resolv.conf':
+    ensure => "file",
+    owner  => "root",
+    group  => "root",
+    mode   => "644",
+    source => "/root/radon_puppet/etc_2/resolv.conf",}   
+    
+file { '/etc/hostname':
+    ensure => "file",
+    owner  => "root",
+    group  => "root",
+    mode   => "644",
+    source => "/root/radon_puppet/etc_2/hostname",}
+    
+file { '/etc/sysconfig/network-scripts/ifcfg-em2':
+    ensure => "file",
+    owner  => "root",
+    group  => "root",
+    mode   => "644",
+    source => "/root/radon_puppet/etc_7/sysconfig/network-scripts/ifcfg-em2",}
+
+file { '/etc/fstab':
+    ensure => "file",
+    owner  => "root",
+    group  => "root",
+    mode   => "644",
+    source => "/root/radon_puppet/etc_7/fstab",}
+ 
+file { '/etc/sysconfig/network-scripts/ifcfg-em1':
+    ensure => "file",
+    owner  => "root",
+    group  => "root",
+    mode   => "644",
+    source => "/root/radon_puppet/etc_7/sysconfig/network-scripts/ifcfg-em1",}   
+ 
+file { '/etc/sysconfig/network-scripts/ifcfg-ib0':
+    ensure => "file",
+    owner  => "root",
+    group  => "root",
+    mode   => "644",
+    source => "/root/radon_puppet/etc_7/sysconfig/network-scripts/ifcfg-ib0",}
+
+file { '/etc/profile.d/mpi.sh':
+    ensure => "file",
+    owner  => "root",
+    group  => "root",
+    mode   => "644",
+    source => "/root/radon_puppet/etc_2/profile.d/mpi.sh",}   
+
+file { '/etc/group':
+    ensure => "file",
+    owner  => "root",
+    group  => "root",
+    mode   => "644",
+    source => "/root/radon_puppet/etc_7/group",}
+
+file { '/etc/passwd':
+    ensure => "file",
+    owner  => "root",
+    group  => "root",
+    mode   => "644",
+    source => "/root/radon_puppet/etc_7/passwd",}
+    
+file { '/etc/slurm/slurm.conf':
+    ensure => "file",
+    owner  => "root",
+    group  => "root",
+    mode   => "644",
+    source => "/root/radon_puppet/etc_mgmt/slurm/slurm.conf",}
+
+cron {'gitPull':
+    command => 'cd /root/radon_puppet && git pull',
+    user    => 'root',
+    hour    => '9',
+    minute  => '31',}
+
+cron {'puppetApply':
+    command => 'cd /root/radon_puppet && puppet apply donny_7.pp',
+    user    => 'root',
+    hour    => '9',
+    minute  => '32',}
+ 
+ }
