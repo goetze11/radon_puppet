dc46a09f82e6f569a90ce7462fedd908ffca3c97
goetze11 <goetzec@seattleu.edu>
2017-07-25 15:14:42 -0700

    Configured e8

--- copyPuppetFiles.sh
+++ copyPuppetFiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-folderNum=7
+folderNum=8
 mkdir ~/radon_puppet/etc_$folderNum/sysconfig
 mkdir ~/radon_puppet/etc_$folderNum/sysconfig/network-scripts
 cp /etc/fstab ~/radon_puppet/etc_$folderNum
