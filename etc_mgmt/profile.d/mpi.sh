7ff83e64baadc366128872c41aa8f6308f190cc6
goetze11 <goetzec@seattleu.edu>
2017-07-25 10:23:04 -0700

    Created new etc folders

---
+++ etc_mgmt/profile.d/mpi.sh
@@ -0,0 +1,6 @@
+export PATH=/usr/lib64/openmpi/bin:$PATH
+export LD_LIBRARY_PATH=/usr/lib64/openmpi/lib:$LD_LIBRARY_PATH
+export PATH=/usr/lib64/openmpi/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
+export LD_LIBRARY_PATH=/usr/lib64/openmpi/lib:
+export PATH=/usr/lib64/openmpi/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
+export LD_LIBRARY_PATH=/usr/lib64/openmpi/lib:
